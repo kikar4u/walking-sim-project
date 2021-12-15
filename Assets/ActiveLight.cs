ed3e891c3d7bc2e0f264d89862f632f5783a2acf
Sylve <sylve.renault@yahoo.fr>
2021-12-15 14:12:52 +0100

    [feature]Lights V0 : ON PEUT ALLUMER LES LIGHTS AVEC E LETS GO

---
+++ Assets/ActiveLight.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveLight : MonoBehaviour
+{
+    private Light lightIntensity;
+    public GameObject pointLight;
+    public AnimationCurve curveAnim;
+    private float curveDeltaTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pointLight != null)
+        {
+            lightIntensity = pointLight.GetComponent<Light>();
+
+        }
+    }
+    public void TurnOnLights()
+    {
+        // Get the current position of the sphere
+        //StartCoroutine(AnimateLights(1.0f));
+        float currentLumen = lightIntensity.intensity;
+        currentLumen += 1 * Time.deltaTime;   // Call evaluate on that time   
+        curveDeltaTime += Time.deltaTime;
+        currentLumen = curveAnim.Evaluate(curveDeltaTime);      // Update the current position of the sphere
+        lightIntensity.intensity = currentLumen;
+        Debug.Log("Intensity" + lightIntensity.intensity);
+    }
+    public IEnumerator AnimateLights(float speed)
+    {
+
+        yield return new WaitForSeconds(1.0f);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                Debug.Log("Helloworld");
+                TurnOnLights();
+            }
+        }
+    }
+}
